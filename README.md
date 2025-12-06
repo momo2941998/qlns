0c4a7a827765a751b3a4d5c4c87675e5f540d1ae
Nguyễn Quang Thành <momo2941998@gmail.com>
2025-12-06 07:43:05 +0700

    update code

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ gen HASH
 ```
 echo -n "your-new-password" | shasum -a 256
 ```
+
+Build & Run Project
+1. Build client
+at client/, change .env and run yarn build
+2. docker compose up
--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,38 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import departmentReducer from '../features/departments/departmentSlice';
 import employeeReducer from '../features/employees/employeeSlice';
 
-const departmentPersistConfig = {
-  key: 'departments',
-  storage,
-  blacklist: ['loading', 'error'], // Không lưu loading và error state
-};
-
-const employeePersistConfig = {
-  key: 'employees',
-  storage,
-  blacklist: ['loading', 'error'], // Không lưu loading và error state
-};
-
-const persistedDepartmentReducer = persistReducer(departmentPersistConfig, departmentReducer);
-const persistedEmployeeReducer = persistReducer(employeePersistConfig, employeeReducer);
-
 export const store = configureStore({
   reducer: {
-    departments: persistedDepartmentReducer,
-    employees: persistedEmployeeReducer,
+    departments: departmentReducer,
+    employees: employeeReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
